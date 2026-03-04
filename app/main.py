92611c9a396fd54ae048256220e4e839b4a26bab
AMMAD DIN <AMMADDIN2@BAGH.MTBC.COM>
2026-03-04 12:56:34 +0500

    added CORS

--- app/main.py
+++ app/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .routers import users, auth, courses, sections, lessons, enrollments, quizzes, quiz_attempts, course_ratings, certificates
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3001"],  # Next.js dev server
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(users.router)
 app.include_router(auth.router)
